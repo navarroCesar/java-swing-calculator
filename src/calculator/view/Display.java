c39763123fd8425bcdf609c9c4a6945dcdacdc6e
Cesar Navarro <cnfrs9@gmail.com>
2025-09-28 19:37:34 -0300

    Add Display class implementation

--- src/calculator/view/Display.java
+++ src/calculator/view/Display.java
@@ -1,10 +1,24 @@
 package calculator.view;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
 
+	private final JLabel label;
+
 	public Display() {
+		setBackground(new Color(46, 49, 50));
+		label = new JLabel("1234,56");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("courier", Font.PLAIN, 25));
+
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+		add(label);
 	}
 }
